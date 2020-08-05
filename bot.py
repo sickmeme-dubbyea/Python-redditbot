4ee9a569f73f5710601e433661210a48325c048c
dubbs <memes@sickme.me>
2020-08-05 14:47:33 -0400

    Project bones established

---
+++ bot.py
@@ -0,0 +1,56 @@
+import praw
+from pushbullet import Pushbullet
+import os
+import time
+from config import token, db_file
+import sqlite3
+from sqlite3 import Error
+
+
+# Create connection to sqlite3 instance
+def db_connection(db_file):
+    connection = 'NULL'
+    try:
+        connection = sqlite3.connect(db_file)
+    except Error:
+        print(Error)
+    return connection
+
+
+# Define Reddit
+reddit = praw.Reddit('bot1', config_interpolation="basic")
+reddit.read_only = True  # set our reddit instance to read only.. realistically was unnecessary
+
+# What are we interested in
+searchList = ['jpc', 'crye', 'criterion', 'rosco']
+
+if not os.path.isfile("posts_checked.txt"):  # check to see if we have a file and not create an array
+    posts_read = []
+else:  # Load the existing file if it already exists
+    with open("posts_checked.txt", "r") as f:
+        posts_read = f.read()
+        posts_read = posts_read.split("\n")
+        posts_read = list(filter(None, posts_read))
+
+# Meat and potatoes
+iteration = 0
+while True:
+    try:
+        for submission in reddit.subreddit("gundeals").new(
+                limit=1000):  # Get the last 1000 from the gun deals subreddit
+            if submission.id not in posts_read:
+                if any(matches in submission.title.lower() for matches in searchList):
+                    pb.push_link(title=submission.title, url=submission.url)
+                    print(submission.title)
+                    posts_read.append(submission.id)  # Add the submission we sent a notification for
+                    # Update our file
+                    with open("posts_checked.txt", "w") as f:
+                        for post_id in posts_read:
+                            f.write(post_id + "\n")
+
+        print("Iteration #: ", iteration)
+        time.sleep(60)
+        iteration += 1
+    except ValueError:
+        print("Oh Shit")
+        time.sleep(60)
